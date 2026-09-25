58de4164765ae37b42197795ebdcb03ea2a5ef96
OEPPS <blancmarcus22@gmail.com>
2026-09-24 22:40:15 -0400

    Configure the hosted Supabase project

--- site/config.js
+++ site/config.js
@@ -1,7 +1,6 @@
 // Configuration publique du client Supabase. Le publishable key est fait pour
 // être utilisé dans un navigateur. Ne jamais mettre une clé « secret » ici.
 window.BOUSOL_CONFIG = {
-  supabaseUrl: 'https://VOTRE-PROJET.supabase.co',
-  publishableKey: 'sb_publishable_REMPLACER',
+  supabaseUrl: 'https://qcrsuqtylhntdcrzbtbh.supabase.co',
+  publishableKey: 'sb_publishable_oL4XmfYsKhVWAQLIej3OTA_YVw-iZPU',
 };
-
